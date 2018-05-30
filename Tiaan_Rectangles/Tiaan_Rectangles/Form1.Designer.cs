2613a49f5bb3e8961cc1d040134714914d9f1921
Tiaan <tiaan.nel@derivco.co.za>
2018-05-30 16:25:27 +0200

    Functionality: Generate random values, Create JSON file

--- Tiaan_Rectangles/Tiaan_Rectangles/Form1.Designer.cs
+++ Tiaan_Rectangles/Tiaan_Rectangles/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tiaan_Rectangles
 {
-  partial class Form1
+  partial class MainApplication
   {
     /// <summary>
     /// Required designer variable.
@@ -28,74 +28,58 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.button1 = new System.Windows.Forms.Button();
-      this.groupBox1 = new System.Windows.Forms.GroupBox();
-      this.label1 = new System.Windows.Forms.Label();
-      this.userInputNumericDropdown = new System.Windows.Forms.NumericUpDown();
-      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.GenerateButton = new System.Windows.Forms.Button();
+      this.InstructionLabel = new System.Windows.Forms.Label();
+      this.UserInputNumericDropdown = new System.Windows.Forms.NumericUpDown();
       this.label2 = new System.Windows.Forms.Label();
-      this.AmountChosenlbl = new System.Windows.Forms.Label();
-      ((System.ComponentModel.ISupportInitialize)(this.userInputNumericDropdown)).BeginInit();
+      this.AmountChosenLabel = new System.Windows.Forms.Label();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.pictureBox2 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.UserInputNumericDropdown)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
       this.SuspendLayout();
       // 
-      // button1
+      // GenerateButton
       // 
-      this.button1.Location = new System.Drawing.Point(388, 54);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(82, 20);
-      this.button1.TabIndex = 0;
-      this.button1.Text = "Generate";
-      this.button1.UseVisualStyleBackColor = true;
-      this.button1.Click += new System.EventHandler(this.button1_Click);
+      this.GenerateButton.Location = new System.Drawing.Point(388, 54);
+      this.GenerateButton.Name = "GenerateButton";
+      this.GenerateButton.Size = new System.Drawing.Size(82, 20);
+      this.GenerateButton.TabIndex = 0;
+      this.GenerateButton.Text = "Generate";
+      this.GenerateButton.UseVisualStyleBackColor = true;
+      this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
       // 
-      // groupBox1
+      // InstructionLabel
       // 
-      this.groupBox1.Location = new System.Drawing.Point(15, 164);
-      this.groupBox1.Name = "groupBox1";
-      this.groupBox1.Size = new System.Drawing.Size(489, 325);
-      this.groupBox1.TabIndex = 2;
-      this.groupBox1.TabStop = false;
-      this.groupBox1.Text = "Horizontal";
+      this.InstructionLabel.AutoSize = true;
+      this.InstructionLabel.Location = new System.Drawing.Point(34, 57);
+      this.InstructionLabel.Name = "InstructionLabel";
+      this.InstructionLabel.Size = new System.Drawing.Size(205, 13);
+      this.InstructionLabel.TabIndex = 4;
+      this.InstructionLabel.Text = "Please enter a number between 3 and 30:";
       // 
-      // label1
+      // UserInputNumericDropdown
       // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(34, 57);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(205, 13);
-      this.label1.TabIndex = 4;
-      this.label1.Text = "Please enter a number between 3 and 30:";
-      // 
-      // userInputNumericDropdown
-      // 
-      this.userInputNumericDropdown.Location = new System.Drawing.Point(245, 54);
-      this.userInputNumericDropdown.Maximum = new decimal(new int[] {
+      this.UserInputNumericDropdown.Location = new System.Drawing.Point(245, 54);
+      this.UserInputNumericDropdown.Maximum = new decimal(new int[] {
             30,
             0,
             0,
             0});
-      this.userInputNumericDropdown.Minimum = new decimal(new int[] {
+      this.UserInputNumericDropdown.Minimum = new decimal(new int[] {
             3,
             0,
             0,
             0});
-      this.userInputNumericDropdown.Name = "userInputNumericDropdown";
-      this.userInputNumericDropdown.Size = new System.Drawing.Size(120, 20);
-      this.userInputNumericDropdown.TabIndex = 5;
-      this.userInputNumericDropdown.Value = new decimal(new int[] {
+      this.UserInputNumericDropdown.Name = "UserInputNumericDropdown";
+      this.UserInputNumericDropdown.Size = new System.Drawing.Size(120, 20);
+      this.UserInputNumericDropdown.TabIndex = 5;
+      this.UserInputNumericDropdown.Value = new decimal(new int[] {
             3,
             0,
             0,
             0});
-      // 
-      // groupBox2
-      // 
-      this.groupBox2.Location = new System.Drawing.Point(510, 164);
-      this.groupBox2.Name = "groupBox2";
-      this.groupBox2.Size = new System.Drawing.Size(489, 325);
-      this.groupBox2.TabIndex = 6;
-      this.groupBox2.TabStop = false;
-      this.groupBox2.Text = "Vertical";
       // 
       // label2
       // 
@@ -105,32 +89,50 @@
       this.label2.Size = new System.Drawing.Size(0, 13);
       this.label2.TabIndex = 7;
       // 
-      // AmountChosenlbl
+      // AmountChosenLabel
       // 
-      this.AmountChosenlbl.AutoSize = true;
-      this.AmountChosenlbl.Location = new System.Drawing.Point(507, 101);
-      this.AmountChosenlbl.Name = "AmountChosenlbl";
-      this.AmountChosenlbl.Size = new System.Drawing.Size(43, 13);
-      this.AmountChosenlbl.TabIndex = 8;
-      this.AmountChosenlbl.Text = "Amount";
-      this.AmountChosenlbl.Visible = false;
+      this.AmountChosenLabel.AutoSize = true;
+      this.AmountChosenLabel.Location = new System.Drawing.Point(507, 101);
+      this.AmountChosenLabel.Name = "AmountChosenLabel";
+      this.AmountChosenLabel.Size = new System.Drawing.Size(43, 13);
+      this.AmountChosenLabel.TabIndex = 8;
+      this.AmountChosenLabel.Text = "Amount";
+      this.AmountChosenLabel.Visible = false;
       // 
-      // Form1
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point(37, 191);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(400, 300);
+      this.pictureBox1.TabIndex = 9;
+      this.pictureBox1.TabStop = false;
+      // 
+      // pictureBox2
+      // 
+      this.pictureBox2.Location = new System.Drawing.Point(479, 191);
+      this.pictureBox2.Name = "pictureBox2";
+      this.pictureBox2.Size = new System.Drawing.Size(400, 300);
+      this.pictureBox2.TabIndex = 10;
+      this.pictureBox2.TabStop = false;
+      // 
+      // MainApplication
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(1012, 580);
-      this.Controls.Add(this.AmountChosenlbl);
+      this.Controls.Add(this.pictureBox2);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.AmountChosenLabel);
       this.Controls.Add(this.label2);
-      this.Controls.Add(this.groupBox2);
-      this.Controls.Add(this.userInputNumericDropdown);
-      this.Controls.Add(this.label1);
-      this.Controls.Add(this.groupBox1);
-      this.Controls.Add(this.button1);
-      this.Name = "Form1";
+      this.Controls.Add(this.UserInputNumericDropdown);
+      this.Controls.Add(this.InstructionLabel);
+      this.Controls.Add(this.GenerateButton);
+      this.Name = "MainApplication";
       this.Text = "Tiaan Nel - Rectangles";
       this.Load += new System.EventHandler(this.Form1_Load);
-      ((System.ComponentModel.ISupportInitialize)(this.userInputNumericDropdown)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.UserInputNumericDropdown)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -138,13 +140,13 @@
 
     #endregion
 
-    private System.Windows.Forms.Button button1;
-    private System.Windows.Forms.GroupBox groupBox1;
-    private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.NumericUpDown userInputNumericDropdown;
-    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.Button GenerateButton;
+    private System.Windows.Forms.Label InstructionLabel;
+    private System.Windows.Forms.NumericUpDown UserInputNumericDropdown;
     private System.Windows.Forms.Label label2;
-    private System.Windows.Forms.Label AmountChosenlbl;
+    private System.Windows.Forms.Label AmountChosenLabel;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.PictureBox pictureBox2;
   }
 }
 
--- Tiaan_Rectangles/Tiaan_Rectangles/Form1.cs
+++ Tiaan_Rectangles/Tiaan_Rectangles/Form1.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace Tiaan_Rectangles
 {
-  public partial class Form1 : Form
+  public partial class MainApplication : Form
   {
-    public Form1()
+    public MainApplication()
     {
       InitializeComponent();
     }
@@ -22,11 +25,63 @@ namespace Tiaan_Rectangles
 
     }
 
-    private void button1_Click(object sender, EventArgs e)
+    private void GenerateButton_Click(object sender, EventArgs e)
     {
-      var amountOfRectangles = userInputNumericDropdown.Value;
-      AmountChosenlbl.Text = "You chose " + amountOfRectangles + " rectangles!";
-      AmountChosenlbl.Visible = true;
+      var amountOfRectangles = UserInputNumericDropdown.Value;
+      AmountChosenLabel.Text = "You chose " + amountOfRectangles + " rectangles!";
+      AmountChosenLabel.Visible = true;
+
+      DeterminInputRectangles(amountOfRectangles);
+    }
+
+    public void DeterminInputRectangles(decimal amountOfRectangles)
+    {
+      int x = 0;
+      int canvasWidth = 400;
+      int canvasHight = 300;
+      int i;
+
+      var rectangles = new List<Rectangle>();
+
+      for (i = 0; i < amountOfRectangles; i++)
+      {
+        var random = new Random();
+        var width = Math.Round(random.NextDouble() * (canvasWidth) + 5);
+        var hight = Math.Round(random.NextDouble() * (canvasHight) + 1);
+        var y = canvasHight - hight;
+
+        rectangles.Add(new Rectangle
+        {
+          Name = "Rectangle" + (i + 1),
+          X = x,
+          Y = (int)y,
+          Width = (int)width,
+          Hight = (int)hight
+        });
+
+        x = (int)(x + width);
+      }
+      WriteInputFile(rectangles);
+    }
+
+    private static void WriteInputFile(List<Rectangle> rectangles)
+    {
+      const string inputFile = "InputRectangles.json";
+      File.WriteAllText(inputFile, JsonConvert.SerializeObject(rectangles));
+      using (StreamWriter file = File.CreateText(inputFile))
+      {
+        JsonSerializer serializer = new JsonSerializer();
+        serializer.Serialize(file, rectangles);
+      }
+    }
+
+    public class Rectangle
+    {
+      public string Name { get; set; }
+      public int X { get; set; }
+      public int Y { get; set; }
+      public int Width { get; set; }
+      public int Hight { get; set; }
     }
   }
 }
